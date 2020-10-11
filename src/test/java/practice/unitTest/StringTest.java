cdd14fd334e9522ae56c5294d7175903b78f73f1
Will Jones <will.jones2013@gmail.com>
2020-10-11 13:10:56 -0700

    initial commit

---
+++ src/test/java/practice/unitTest/StringTest.java
@@ -0,0 +1,19 @@
+package practice.unitTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class StringTest {
+
+    @Test
+    public void test() {
+        fail("not done yet");
+    }
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+}
